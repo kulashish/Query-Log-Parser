1303bbec991e339a09f2cb845206175eac0a8415
Ashish Kulkarni <kulashish@gmail.com>
2011-12-05 15:03:36 +0530

    command line inputs

--- src/com/iit/projects/search/QueryLogParser.java
+++ src/com/iit/projects/search/QueryLogParser.java
@@ -1,24 +1,44 @@
 package com.iit.projects.search;
 
+
 public class QueryLogParser {
 
-	private static final String QUERYLOG_FILE = "C:\\Documents and Settings\\Administrator\\My Documents\\Search\\combined-sample.log";
-	private static final String OUTPUT_FILE = "C:\\Documents and Settings\\Administrator\\My Documents\\Search\\combined-sample_out.log";
+	// private static final String QUERYLOG_FILE =
+	// "C:\\Documents and Settings\\Administrator\\My Documents\\Search\\combined-sample.log";
+	// private static final String OUTPUT_FILE =
+	// "C:\\Documents and Settings\\Administrator\\My Documents\\Search\\combined-sample_out.log";
 	private static final String[] SPAM_WORDS = { "rediff", "facebook", "movie" };
 	private static final String QUERY_STRING = "search?";
 	private static final String HTML_CONTENT = "text/html";
+	private static final int NUMBER_ARGS = 2;
+
+	private String queryLogFile;
+	private String outFile;
+
+	public QueryLogParser() {
+
+	}
+
+	public QueryLogParser(String inFilePath, String outFilePath) {
+		queryLogFile = inFilePath;
+		outFile = outFilePath;
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new QueryLogParser().parse(QUERYLOG_FILE);
+		if (args.length != NUMBER_ARGS) {
+			System.err.println("Usage: QueryLogParser <inlogfile> <outfile>");
+			System.exit(1);
+		}
+		new QueryLogParser(args[0], args[1]).parse();
 	}
 
-	public void parse(String filePath) {
+	public void parse() {
 		QueryLog log = null;
 		try {
-			log = new QueryLog(filePath);
+			log = new QueryLog(queryLogFile);
 			QueryLogLineFilter spamFilter = new QueryURLFilter();
 			spamFilter.setFilterText(SPAM_WORDS);
 
@@ -43,7 +63,7 @@ public class QueryLogParser {
 			e.printStackTrace();
 		}
 		try {
-			log.unmarshall(OUTPUT_FILE);
+			log.unmarshall(outFile);
 		} catch (QueryLogOutputException e) {
 			e.printStackTrace();
 		}
